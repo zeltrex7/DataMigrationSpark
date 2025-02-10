896c3b50c8aef3f8984512c618a14a528ae6df58
zeltrex7 <93510067+zeltrex7@users.noreply.github.com>
2025-02-10 18:28:56 +0530

    end to end pipeline

--- airflow/dags/dag_metadata_ingestion.py
+++ airflow/dags/dag_metadata_ingestion.py
@@ -9,8 +9,19 @@ default_args = {
 
 dag = DAG('spark_submit_via_docker', default_args=default_args, schedule_interval=None)
 
-bash_task = BashOperator(
-    task_id="submit_spark_job",
+
+metadata_ingestion = BashOperator(
+    task_id="metadata_ingestion",
+    bash_command="""docker exec da-spark-master spark-submit --jars /opt/spark/apps/jars/mysql-connector-java-8.0.30.jar,/opt/spark/apps/jars/mssql-jdbc-12.8.1.jre8.jar \
+                --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/opt/spark/apps/log4j.properties" \
+                --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/opt/spark/apps/log4j.properties" \
+                /opt/spark/apps/scripts/MetadataIngestion.py""",
+    dag=dag
+
+)
+
+data_ingestion = BashOperator(
+    task_id="data_ingestion",
     bash_command="""docker exec da-spark-master spark-submit --jars /opt/spark/apps/jars/mysql-connector-java-8.0.30.jar,/opt/spark/apps/jars/mssql-jdbc-12.8.1.jre8.jar \
                 --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/opt/spark/apps/log4j.properties" \
                 --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/opt/spark/apps/log4j.properties" \
@@ -19,3 +30,5 @@ bash_task = BashOperator(
 
 )
 
+
+metadata_ingestion >> data_ingestion
